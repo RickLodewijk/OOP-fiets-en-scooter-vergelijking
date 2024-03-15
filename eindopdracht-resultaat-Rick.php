adb857b6ace40b2c1911707e5bea1f59a6e95f0b
Rick Lodewijk <ricklodewijk99@gmail.com>
2024-03-15 10:41:22 +0100

    Done

---
+++ eindopdracht-resultaat-Rick.php
@@ -0,0 +1,18 @@
+<?php
+    class Weg{
+        public $file;
+        public $lengte;
+    }
+    class Voertuig{
+        $bandenspanning;
+        $snelheid;
+    }
+    class Fiets extends Voertuig {
+
+    }
+    class Scooter extends Voertuig {
+
+    }
+    
+    
+?>
